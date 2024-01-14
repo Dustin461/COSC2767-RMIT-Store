df3a687a971beff48e3e6dd0ee6f9ca4edfa2213
Duy Anh Pham <s3802674@student.rmit.edu.au>
2024-01-15 03:08:47 +0700

    Connect to Maria DB

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "localhost",
+                  "44.196.17.90:8081",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
